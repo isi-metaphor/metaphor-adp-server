3c230a35ee8a7800d6fb4be03382208daf372e4c
Vova Zaytsev <zaytsev@usc.edu>
2014-03-14 18:15:22 -0700

    Remove env vars export

--- fab/paths.py
+++ fab/paths.py
@@ -10,7 +10,9 @@ import os
 import sys
 import logging
 
+
 logger = logging.getLogger("pipeline")
+
 
 METAPHOR_DIR        =  "{{PIPELINE_CONFIG.METAPHOR_DIR}}"
 HENRY_DIR           =  "{{PIPELINE_CONFIG.HENRY_DIR}}"
@@ -35,7 +37,6 @@ ES_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Spanish/Spanish_compile
 RU_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Russian/Russian_compiled_KB.da")
 FA_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Farsi/Farsi_compiled_KB.da")
 
-
 CODE_RU             =   os.path.join(METAPHOR_DIR, "/pipelines/Russian/")
 CODE_EN             =   os.path.join(METAPHOR_DIR, "/pipelines/English/")
 CODE_ES             =   os.path.join(METAPHOR_DIR, "/pipelines/Spanish/")
@@ -43,34 +44,9 @@ CODE_FA             =   os.path.join(METAPHOR_DIR, "/pipelines/Farsi/")
 CODE_COMMON         =   os.path.join(METAPHOR_DIR, "/pipelines/common/")
 
 
-# os.environ["METAPHOR_DIR"]          =   METAPHOR_DIR
-# os.environ["HENRY_DIR"]             =   HENRY_DIR
-# os.environ["BOXER_DIR"]             =   BOXER_DIR
-# os.environ["TMP_DIR"]               =   TMP_DIR
-# os.environ["GUROBI_HOME"]           =   GUROBI_HOME
-# os.environ["GRB_LICENSE_FILE"]      =   GRB_LICENSE_FILE
-# os.environ["CPLUS_INCLUDE_PATH"]    =   CPLUS_INCLUDE_PATH
-# os.environ["PATH"]                  =   PATH
-# os.environ["LD_LIBRARY_PATH"]       =   LD_LIBRARY_PATH
-# os.environ["LIBRARY_PATH"]          =   LIBRARY_PATH
-
-# os.environ["PYTHONPATH"]            =   "%s:%s:%s:%s:%s:$PYTHONPATH" % (
-#     CODE_RU,
-#     CODE_EN,
-#     CODE_ES,
-#     CODE_FA,
-#     CODE_COMMON,
-# )
-
 sys.path.extend([CODE_RU, CODE_EN, CODE_ES, CODE_FA, CODE_COMMON])
 
 
 logger.info("Envieronment variables: %s." % "\n".join([
     "%s=%s" % (k, v) for k, v in os.environ.items()
 ]))
-
-logger.info("\n\n\nDONE\n\n\n")
-
-for i in xrange(10000):
-    import os
-    logger.info("User: %r" % os.getusername())
