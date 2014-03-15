5445de8f45ef20e781d42159ad8a75e91ea41ea3
Vova Zaytsev <zaytsev@usc.edu>
2014-03-14 17:24:45 -0700

    Fix exporting env variables

--- fab/paths.py
+++ fab/paths.py
@@ -7,6 +7,10 @@
 # For license information, see LICENSE
 
 import os
+import sys
+import logging
+
+logger = logging.getLogger("pipeline")
 
 METAPHOR_DIR        =  "{{PIPELINE_CONFIG.METAPHOR_DIR}}"
 HENRY_DIR           =  "{{PIPELINE_CONFIG.HENRY_DIR}}"
@@ -31,6 +35,14 @@ ES_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Spanish/Spanish_compile
 RU_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Russian/Russian_compiled_KB.da")
 FA_KBPATH           =   os.path.join(METAPHOR_DIR, "/KBs/Farsi/Farsi_compiled_KB.da")
 
+
+CODE_RU             =   os.path.join(METAPHOR_DIR, "/pipelines/Russian/")
+CODE_EN             =   os.path.join(METAPHOR_DIR, "/pipelines/English/")
+CODE_ES             =   os.path.join(METAPHOR_DIR, "/pipelines/Spanish/")
+CODE_FA             =   os.path.join(METAPHOR_DIR, "/pipelines/Farsi/")
+CODE_COMMON         =   os.path.join(METAPHOR_DIR, "/pipelines/common/")
+
+
 os.environ["METAPHOR_DIR"]          =   METAPHOR_DIR
 os.environ["HENRY_DIR"]             =   HENRY_DIR
 os.environ["BOXER_DIR"]             =   BOXER_DIR
@@ -41,3 +53,16 @@ os.environ["CPLUS_INCLUDE_PATH"]    =   CPLUS_INCLUDE_PATH
 os.environ["PATH"]                  =   PATH
 os.environ["LD_LIBRARY_PATH"]       =   LD_LIBRARY_PATH
 os.environ["LIBRARY_PATH"]          =   LIBRARY_PATH
+
+os.environ["PYTHONPATH"]            =   "%s:%s:%s:%s:%s:$PYTHONPATH" % (
+    CODE_RU,
+    CODE_EN,
+    CODE_ES,
+    CODE_FA,
+    CODE_COMMON,
+)
+
+sys.path.extend([CODE_RU, CODE_EN, CODE_ES, CODE_FA, CODE_COMMON])
+
+
+logger.info("Envieronment variables: %s." % "\n".join(os.environ))
