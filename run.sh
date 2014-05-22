6c159c6fe1c52bb0bfa1b50c3584db902eb52961
amiparikh <amiakshp@usc.edu>
2014-05-22 14:41:05 -0700

    email and sleep

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@
 while [ 1 == 1 ]
 do
 	python manage.py runserver 0.0.0.0:8081 --settings=lccsrv.settings
+	SUBJECT="Dev Server Error"
+	EMAIL="amiakshp@usc.edu"
+	EMAILMESSAGE="/tmp/emailmessage.txt"
+	echo "Dev Server is down" > $EMAILMESSAGE
+	mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+	sleep 30s
 done
