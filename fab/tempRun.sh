d75a584cd1c6b39de3933e1ebd25bf7a8018a453
amiparikh <amiakshp@usc.edu>
2014-10-08 10:52:40 -0700

    simplified fabfile, removed uwsgi and nginx that do not seem to be used. generalized config by requiring a parameter to specify the branch to get.

---
+++ fab/tempRun.sh
@@ -0,0 +1,1 @@
+python manage.py runserver 0.0.0.0:{{NGINX_PORT}} --settings=lccsrv.settings
--- fabfile.py
+++ fabfile.py
@@ -17,69 +17,30 @@ from fabric.api import *
 from fabric.colors import green, red
 
 
-def dev():
+def basicConfig(branch):
     env.config = {
         "repository":   "https://github.com/isi-metaphor/lcc-service.git",
-        "branch":       "dev",
-        "context":       json.load(open("fab/config.dev.json", "r")),
+        "branch":       branch,
+        "context":       json.load(open("fab/config."+branch+".json", "r")),
     }
     env.config["path"] = env.config["context"]["ROOT"]
     env.config["stage"] = env.config["context"]["STAGE"]
 
-
-def prod():
-    env.config = {
-        "repository":   "https://github.com/isi-metaphor/lcc-service.git",
-        "branch":       "master",
-        "context":      json.load(open("fab/config.prod.json", "r")),
-    }
-    env.config["path"] = env.config["context"]["ROOT"]
-    env.config["stage"] = env.config["context"]["STAGE"]
-
-
-def run_local():
+def localConfig():
     env.host_string = "localhost"
-    env.user = "zvm"
+    env.user = "metaphor"
     env.local = True
-    local("pypy manage.py runserver 0.0.0.0:8000 --settings=settings")
 
-
-def server():
-    env.host_string = "colo-vm19.isi.edu"
+def remoteConfig():
+    env.host_string = "colo-pm4.isi.edu"
     env.user = "metaphor"
     env.key_filename = "~/.ssh/id_dsa"
     env.local = False
 
-
-def init():
-    if not env.local:
-        run("sudo aptitude -f install %s" % " ".join((
-            "git",
-            "uwsgi",
-            "nginx",
-            "python",
-            "sqlite3",
-            "sqlite3-dev",
-            "uwsgi-plugin-python",
-        )))
-        if not fabric.contrib.files.exists(env.config["path"]):
-            run("git clone {repository} -b {branch} {path}".format(**env.config))
-
-
-def commit():
-     local("git add -A")
-     local("git diff --quiet --exit-code --cached || git commit -m 'Update'")
-     local("git push")
-     print(green('Committed and pushed to git.', bold=False))
-
-
-def update():
-    print(green("Updating packages."))
-    run("sudo pip install -r requirements.txt")
-
-
 def deploy():
 
+    if not fabric.contrib.files.exists(env.config["path"]):
+        run("git clone {repository} -b {branch} {path}".format(**env.config))
     env.lcwd = os.path.dirname(__file__)
 
     config = env.config
@@ -103,7 +64,6 @@ def deploy():
                                              "{path}/bashrc.sh".format(**config),
                                              context=context,
                                              use_jinja=True)
-        run("cp -f {path}/bashrc.sh ~/metaphor.sh".format(**config))
 
         print(green("Uploading setting.py"))
         fabric.contrib.files.upload_template("fab/settings.py",
@@ -114,24 +74,14 @@ def deploy():
                                              "{path}/lccsrv/paths.py".format(**config),
                                              context=context,
                                              use_jinja=True)
-
-        print(green("Uploading usgi.ini"))
-        fabric.contrib.files.upload_template("fab/uwsgi.ini",
-                                             "{path}/uwsgi.ini".format(**config),
+        fabric.contrib.files.upload_template("fab/tempRun.sh",
+                                             "{path}/tempRun.sh".format(**config),
                                              context=context,
                                              use_jinja=True)
-        run("sudo cp -f {path}/uwsgi.ini /etc/uwsgi/apps-available/{stage}.ini".format(**config))
-        run("sudo ln -sf /etc/uwsgi/apps-available/{stage}.ini /etc/uwsgi/apps-enabled/{stage}.ini".format(**config))
-        run("sudo /etc/init.d/uwsgi stop {stage}".format(**config))
-
-        print(green("Uploading nginx config"))
-        fabric.contrib.files.upload_template("fab/nginx.conf",
-                                             "{path}/nginx.conf".format(**config),
+        fabric.contrib.files.upload_template("fab/shell.sh",
+                                             "{path}/shell.sh".format(**config),
                                              context=context,
                                              use_jinja=True)
-        run("sudo cp -f {path}/nginx.conf /etc/nginx/sites-available/{stage}".format(**config))
-        run("sudo ln -sf /etc/nginx/sites-available/{stage} /etc/nginx/sites-enabled/{stage}".format(**config))
-        run("sudo /etc/init.d/nginx stop".format(**config))
 
         # print(green("Syncing database."))
         # # run("python manage.py syncdb --noinput")
@@ -139,20 +89,9 @@ def deploy():
         # print(green("Creating indexes."))
         # run("python manage.py syncdb --noinput --settings=lccsrv.settings")
 
-
-def test_client():
-    port = env.config["context"]["NGINX_PORT"]
-    host = env.config["context"]["NGINX_SERVER_NAME"]
-    for file_path in glob.glob("testdata/queries/*.json"):
-        cmd = "python oldclient/client.py -g %s -p %d -j %s" % (host, port, file_path)
-        print(green(cmd))
-        run(cmd)
-
-
-def devdeploy():
-    dev()
-    server()
-    #init()
+def install(branch):
+    basicConfig(branch)
+    localConfig()
     deploy()
 
 def generateBashConfig(file):
@@ -173,9 +112,3 @@ def generateBashConfig(file):
     fo.write("export LIBRARY_PATH=${GUROBI_HOME}/lib\n")
     fo.close()
 
-
-def proddeploy():
-    prod()
-    server()
-    #init()
-    deploy()
