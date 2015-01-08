d5bce9e20d7696920402bff5fc2b34478859bc75
fmorbini <fmorbini@gmail.com>
2015-01-08 11:41:34 -0800

    continue improving running and testing scripts

--- fab/checkService.sh
+++ fab/checkService.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+yest_date=`date "+%s"`
+EMAILS="fmorbini@gmail.com morbini@ict.usc.edu"
+SUBJECT=`hostname`":"`pwd`": {{STAGE}}_{{NGINX_PORT}}"
+outputFile="/tmp/${$}-`date +%s`.tmp"
+
 runCheck() {
-        python oldclient/client.py -g 0.0.0.0 -p $port -j checkServiceFiles/$1.json > $outputFile
+        python oldclient/client.py -g 0.0.0.0 -p {{NGINX_PORT}} -j checkServiceFiles/$1.json > $outputFile
 	if diff checkServiceFiles/$1_output.json $outputFile; then
 		sleep 20
 	else
 		body="Response JSON did not match the expected JSON!"
 		process=$(ps fax|grep -e"^[ \t]*$$" -C10)
 		langSubject=$1": "$SUBJECT
-		echo "$body\n$process" | mail -s \"$langSubject\" $EMAILS
+		echo "$body\n$process" | mail -s "$langSubject" $EMAILS
 		sleep 5m
 	fi
 }
 
-port=$(($1+0))
-yest_date=`date "+%s"`
-EMAILS="fmorbini@gmail.com morbini@ict.usc.edu"
-SUBJECT=`hostname`":"`pwd`": {{STAGE}}_{{NGINX_PORT}}"
-outputFile="/tmp/outputCheckAugEval.json"
 while [ 1 == 1 ]
 do
     runCheck "en"
@@ -27,7 +27,7 @@ do
     time_diff=$((`date "+%s"` - $yest_date)) 
     if [ $time_diff -gt 86400 ]; then
 	yest_date=`date "+%s"`
-	echo ""|mail -s \""checkServiceScript for "$SUBJECT" is running."\" "$EMAILS"
+	echo ""|mail -s "checkServiceScript for $SUBJECT is running." $EMAILS
     fi
     sleep 5m
 done
