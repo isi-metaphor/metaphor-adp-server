14c8b6ec72ba0cc7f3c2e0708bd5faa325cb1d13
fmorbini <fmorbini@gmail.com>
2015-01-08 11:26:43 -0800

    continue improving startup scripts

--- fab/run.sh
+++ fab/run.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-port=8084
-./checkService.sh $port &
+./checkService.sh {{NGINX_PORT}} &
+
+SUBJECT=`hostname`":"`pwd`": {{STAGE}}_{{NGINX_PORT}}"
+EMAILS="fmorbini@gmail.com morbini@ict.usc.edu"
 
 while [ 1 == 1 ]
 do
-	#sleep 1s
-	#continue
-	python manage.py runserver 0.0.0.0:8084 --settings=lccsrv.settings
-	SUBJECT="Aug-Eval Server Error"
-	EMAIL1="amiakshp@usc.edu"
-	EMAIL2="morbini@ict.usc.edu"
-	EMAILMESSAGE="/tmp/emailmessage.txt"
-	echo "Aug-Eval Server is down" > $EMAILMESSAGE
-	process=$(ps fax|grep -e"^[ \t]*$$" -C10)
-	echo "$process" >> $EMAILMESSAGE
-	mail -s "$SUBJECT" "$EMAIL1" "$EMAIL2" < $EMAILMESSAGE
+	python manage.py runserver 0.0.0.0:{{NGINX_PORT}} --settings=lccsrv.settings
+	echo ""|mail -s \""SERVICE DIED will restart: "$SUBJECT\" "$EMAILS"
 	sleep 30s
 done
