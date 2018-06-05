94029f1f20c36cf37a6f42d131d30c2988717a10
Jonathan Gordon <jgordon@isi.edu>
2018-06-04 17:17:02 -0700

    Specify Python 2.7; use $(..) instead of legacy `..`

--- fab/run.sh
+++ fab/run.sh
@@ -2,12 +2,12 @@
 
 ./checkService.sh &
 
-SUBJECT=`hostname`":"`pwd`": {{STAGE}}_{{NGINX_PORT}}"
+SUBJECT="$(hostname):$(pwd): {{STAGE}}_{{NGINX_PORT}}"
 
 while [ 1 == 1 ]
 do
-	python manage.py runserver 0.0.0.0:{{NGINX_PORT}} \
+    python2.7 manage.py runserver 0.0.0.0:{{NGINX_PORT}} \
            --settings=lccsrv.settings
-	echo "" | mail -s "SERVICE DIED will restart: $SUBJECT" $EMAILS
-	sleep 30s
+    echo "" | mail -s "SERVICE DIED will restart: $SUBJECT" $EMAILS
+    sleep 30s
 done
